a7943813f9ea590ccb0695df3c1973a88313531d
otreblan <otreblain@gmail.com>
2022-06-01 17:04:24 -0500

    Fill template

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-// A C++ template
-// Copyright © 2019-2020 otreblan
+// Proyecto-3
+// Copyright © 2022 Grupo 7
 //
-// hello is free software: you can redistribute it and/or modify
+// proyecto-3 is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// hello is distributed in the hope that it will be useful,
+// proyecto-3 is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
